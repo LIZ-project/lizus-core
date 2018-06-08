a9390386b326390ceebf59344a8740ddd6dc7978
Nillecram <lokytech@gmail.com>
2018-06-08 22:44:41 +0200

    Cuts communication with the old network
    Cuts for proto < 30006.
    It's a brand new day and the sun is high.

--- src/version.h
+++ src/version.h
@@ -30,25 +30,25 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 30005;
+static const int PROTOCOL_VERSION = 30006;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 30003;
-static const int MIN_PEER_PROTO_VERSION_2 = 30004;
+static const int MIN_PEER_PROTO_VERSION_2 = 30006;
 
 // minimum peer version accepted by DarkSendPool
-static const int MIN_POOL_PEER_PROTO_VERSION = 30005;
+static const int MIN_POOL_PEER_PROTO_VERSION = 30006;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 30005;
+static const int MIN_INSTANTX_PROTO_VERSION = 30006;
 
 //! minimum peer version that can receive masternode payments
 // V1 - Last protocol version before update
 // V2 - Newest protocol version
 static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 30003;
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 30004;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 30006;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -56,7 +56,7 @@ static const int CADDR_TIME_VERSION = 10000;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 30000;
-static const int NOBLKS_VERSION_END = 30004;
+static const int NOBLKS_VERSION_END = 30005;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 10000;
